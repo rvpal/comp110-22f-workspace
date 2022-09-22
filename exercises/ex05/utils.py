909140d2da80ed0bea3f12c63eb3670039497c57
Ranbir Pal <rpal@unc.edu>
2022-09-21 22:14:55 -0400

    Testing.

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -3,3 +3,35 @@
 __author__ = "730471791"
 
 
+def only_evens(int_list: list[int]) -> list[int]:
+    """Returns a list of evens from a list of even and odds."""
+    even_list: list[int] = list()
+    for i in int_list:
+        if i % 2 == 0:
+            even_list.append(i)
+    return even_list
+
+
+def concat(list_one: list[int], list_two: list[int]) -> list[int]:
+    """Returns a combined list from two lists."""
+    combined_list: list[int] = list()
+    for i in list_one:
+        combined_list.append(i)
+    for i in list_two:
+        combined_list.append(i)
+    return combined_list
+
+
+def sub(int_list: list[int], start: int, end: int) -> list[int]:
+    """Returns a subset of a list given indices."""
+    subset: list[int] = list()
+    if len(int_list) == 0 or start > len(int_list) or end <= 0:
+        return subset
+    if end > len(int_list):
+        end = len(int_list)
+    if start < 0:
+        start = 0
+    while start < end:
+        subset.append(int_list[start])
+        start += 1
+    return subset
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,8 +3,89 @@
 __author__ = "730471791"
 
 
-from exercises.ex05.utils import only_evens, concat, sub
+from utils import only_evens, concat, sub
 
 
-# make three test functions for each function. 
-# One edge case two use cases. 
+def test_only_evens_edge() -> None:
+    """Tests an edge case for only_evens."""
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_only_evens_case_one() -> None:
+    """Tests a use case for only_evens."""
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    assert only_evens(xs) == [2, 4, 6]
+
+
+def test_only_evens_case_two() -> None:
+    """Tests another use case for only_evens."""
+    xs: list[int] = [2, 4, 6, 8]
+    assert only_evens(xs) == [2, 4, 6, 8]
+
+
+def test_only_evens_case_three() -> None:
+    """Tests a third use case for only_evens."""
+    xs: list[int] = [1, 3, 5, 7]
+    assert only_evens(xs) == []
+
+
+def test_concat_edge() -> None:
+    """Tests an edge case for concat."""
+    xs: list[int] = []
+    ys: list[int] = []
+    assert concat(xs, ys) == []
+
+
+def test_concat_case_one() -> None:
+    """Tests a use case for concat."""
+    xs: list[int] = [1, 2, 3, 4]
+    ys: list[int] = [5, 6, 7, 8]
+    assert concat(xs, ys) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_concat_case_two() -> None:
+    """Tests another use case for concat."""
+    xs: list[int] = [-10, 3, 4, 2]
+    ys: list[int] = [-5, 1, 5, 9]
+    assert concat(xs, ys) == [-10, 3, 4, 2, -5, 1, 5, 9]
+
+
+def test_sub_edge() -> None:
+    """Tests an edge case for sub."""
+    xs: list[int] = []
+    start: int = 100
+    end: int = -5
+    assert sub(xs, start, end) == []
+
+
+def test_sub_edge_two() -> None:
+    """Tests another edge case for sub."""
+    xs: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    start: int = 100
+    end: int = -5
+    assert sub(xs, start, end) == []
+
+
+def test_sub_edge_three() -> None:
+    """Tests a third edge case for sub."""
+    xs: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    start: int = -1
+    end: int = 100
+    assert sub(xs, start, end) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+def test_sub_case_one() -> None:
+    """Tests a use case for sub."""
+    xs: list[int] = [1, 2, 3, 4, 5]
+    start: int = 2
+    end: int = 4
+    assert sub(xs, start, end) == [3, 4]
+
+
+def test_sub_case_two() -> None:
+    """Tests another use case for sub."""
+    xs: list[int] = [1, 4, 6, 10]
+    start: int = 1
+    end: int = 7
+    assert sub(xs, start, end) == [4, 6, 10]
